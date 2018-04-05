c3c32c0f12b83f3d23d37a451abeece3eba8017c
Magda Czaicka-Chmielewska <czaicka.chmielewska@op.pl>
2018-04-05 17:18:42 +0200

    Add  update

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 function getTriangleArea(a, h) { 
-  if ((a < 0) && (h < 0)) {
-     return "Nieprawidłowe dane";
-  } else {
+  if ((a > 0) && (h > 0)) {
      return a * h / 2;
+  } else {
+     return "Nieprawidłowe dane";
   }
   };
 
@@ -11,8 +11,10 @@ function getTriangleArea(a, h) {
 
 console.log(getTriangleArea(10, 6));
 
-var triangleArea1 = getTriangleArea(10, 10);
+var triangleArea1 = getTriangleArea(-4, 10);
 var triangleArea2 = getTriangleArea(3, 9);
-var triangleArea3 = getTriangleArea(19, 8);
+var triangleArea3 = getTriangleArea(19, 0);
 
-console.log(triangleArea3);
+console.log(triangleArea1);
+console.log(triangleArea2)
+console.log(triangleArea3)
